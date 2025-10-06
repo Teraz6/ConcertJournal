c65b69e5cbf843ffcd25928d69ff86dea0f32199
Teraz6 <retenk@gmail.com>
2025-10-06 21:45:00 +0300

    Change files

--- ConcertJournal/ConcertJournal/MainPage.xaml.cs
+++ ConcertJournal/ConcertJournal/MainPage.xaml.cs
@@ -1,13 +1,17 @@
-﻿namespace ConcertJournal
+﻿using ConcertJournal.Views;
+
+namespace ConcertJournal
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private async void OnAddConcertClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddConcertPage());
+        }
     }
-
 }
