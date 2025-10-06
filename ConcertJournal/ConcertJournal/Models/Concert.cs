7a6b73dcd53ac4c9755fbe21e67d83b45b1274c3
Teraz6 <retenk@gmail.com>
2025-10-06 14:07:56 +0300

    Update Concert.Model

--- ConcertJournal/ConcertJournal/Models/Concert.cs
+++ ConcertJournal/ConcertJournal/Models/Concert.cs
@@ -1,6 +1,20 @@
-﻿namespace ConcertJournal.Models
+﻿using SQLite;
+
+namespace ConcertJournal.Models
 {
     public class Concert
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string? BandName { get; set; }
+        public string? Venue { get; set; }
+        public string? Location { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Notes { get; set; }
+
+        //For future update
+        //public string? PhotoPath { get; set; }
+        //public string? VideoPath { get; set; }
+
     }
 }
