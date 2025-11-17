9ed1996ba82c23102d148374d78baad4aad8d162
Teraz6 <retenk@gmail.com>
2025-11-17 09:57:56 +0200

    Change Page animations and add NavigationHelper file

---
+++ ConcertJournal/ConcertJournal/Services/NavigationHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace ConcertJournal.Services
+{
+    public static class NavigationHelper
+    {
+        public static readonly Dictionary<string, int> PageOrder = new()
+    {
+        { "//MainPage", 0 },
+        { "//ConcertListPage", 1 },
+        { "//AddConcertPage", 2 },
+        { "//StatisticsPage", 3 },
+        { "//SettingsPage", 4 }
+    };
+
+        public static int CurrentIndex = 0;
+    }
+}
--- ConcertJournal/ConcertJournal/Views/Controls/BottomNavBar.xaml.cs
+++ ConcertJournal/ConcertJournal/Views/Controls/BottomNavBar.xaml.cs
@@ -1,3 +1,4 @@
+using ConcertJournal.Services;
 using System.Windows.Input;
 
 namespace ConcertJournal.Views.Controls;
@@ -19,26 +20,57 @@ public partial class BottomNavBar : ContentView
 
     private async void OnHomeClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync($"//MainPage");
+        await NavigateWithAnimation("//MainPage",
+            NavigationHelper.PageOrder["//MainPage"]);
     }
 
     private async void OnListClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync($"//ConcertListPage");
+        await NavigateWithAnimation("//ConcertListPage",
+            NavigationHelper.PageOrder["//ConcertListPage"]);
     }
 
     private async void OnAddClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync($"//AddConcertPage");
+        await NavigateWithAnimation("//AddConcertPage",
+            NavigationHelper.PageOrder["//AddConcertPage"]);
     }
 
     private async void OnSettingsClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync($"//SettingsPage");
+        await NavigateWithAnimation("//SettingsPage",
+            NavigationHelper.PageOrder["//SettingsPage"]);
     }
 
     private async void OnStatisticsClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync($"//StatisticsPage");
+        await NavigateWithAnimation("//StatisticsPage",
+            NavigationHelper.PageOrder["//StatisticsPage"]);
+    }
+
+    private async Task NavigateWithAnimation(string route, int newIndex)
+    {
+        int oldIndex = NavigationHelper.CurrentIndex;
+
+        bool goingRight = newIndex > oldIndex;
+
+        // Apply direction-based animation
+        Shell.Current.CustomShellMaui(new CustomShellMaui.Models.Transitions
+        {
+            Push = new CustomShellMaui.Models.Transition
+            {
+                CurrentPage = goingRight
+                    ? CustomShellMaui.Enum.TransitionType.LeftOut
+                    : CustomShellMaui.Enum.TransitionType.RightOut,
+
+                NextPage = goingRight
+                    ? CustomShellMaui.Enum.TransitionType.RightIn
+                    : CustomShellMaui.Enum.TransitionType.LeftIn
+            }
+        });
+
+        NavigationHelper.CurrentIndex = newIndex;
+
+        await Shell.Current.GoToAsync(route);
     }
 }
