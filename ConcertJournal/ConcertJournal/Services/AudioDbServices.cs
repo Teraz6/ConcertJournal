98b8c55fb3883e85d8a85e76e8c36ddc37976239
Teraz6 <retenk@gmail.com>
2026-01-26 17:35:20 +0200

    Create AudioDbServices

---
+++ ConcertJournal/ConcertJournal/Services/AudioDbServices.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Json;
+using static ConcertJournal.Models.AudioDbInfo;
+
+namespace ConcertJournal.Services
+{
+    public class AudioDbServices
+    {
+        private readonly HttpClient _httpClient = new();
+        private const string BaseUrl = "https://www.theaudiodb.com/api/v1/json/123/search.php?s=";
+
+        public async Task<ArtistInfo?> GetArtistDetailsAsync(string artistName)
+        {
+            try
+            {
+                var encodedName = Uri.EscapeDataString(artistName);
+                var response = await _httpClient.GetFromJsonAsync<AudioDbResponse>(BaseUrl + encodedName);
+
+                return response?.Artists?.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error fetching artist: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
