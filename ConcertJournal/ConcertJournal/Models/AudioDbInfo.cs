b292621bc2cfdbbc93692651724b0c8fda506ed8
Teraz6 <retenk@gmail.com>
2026-01-26 17:35:09 +0200

    Create AudioDbInfo model

---
+++ ConcertJournal/ConcertJournal/Models/AudioDbInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConcertJournal.Models
+{
+    public class AudioDbInfo
+    {
+        public record AudioDbResponse(List<ArtistInfo> Artists);
+
+        public record ArtistInfo(
+            [property: JsonPropertyName("idArtist")] string IdArtist,
+            [property: JsonPropertyName("strArtist")] string StrArtist,
+            [property: JsonPropertyName("strBiographyEN")] string StrBiographyEN,
+            [property: JsonPropertyName("strArstistFanart")] string StrArtistFanart, //Background image
+            [property: JsonPropertyName("strArtistThumb")] string StrArtistThumb,  //Square profile pic
+            [property: JsonPropertyName("strArtistBanner")] string StrArtistBanner //Banner image
+        );
+    }
+}
